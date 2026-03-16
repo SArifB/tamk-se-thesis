e8eeb20ddeeb721e2b6ebfe0e7bd47bdc49493e7
Arif Basharyar <saye.arif@gmail.com>
2026-03-16 19:52:04 +0200

    added cpp-app1

---
+++ projects/cpp-app1/src/cpp-app1/main.cpp
@@ -0,0 +1,8 @@
+#include "cpp-app1/my-print.hpp"
+
+int main() {
+  using namespace cpp_app1;
+
+  my_print("Hello from src/cpp-app1/main.cpp");
+  return 0;
+}
---
+++ projects/cpp-app1/src/cpp-app1/my-print.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string_view>
+
+namespace cpp_app1 {
+
+auto my_print(std::string_view str) -> void;
+
+}  // namespace cpp_app1
