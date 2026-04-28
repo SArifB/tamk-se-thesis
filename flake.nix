3d4d9908361f4afcbe26d95256a71f4cdc296831
Arif Basharyar <saye.arif@gmail.com>
2026-04-28 17:28:58 +0300

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "thesis monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      # self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            # Build tools
+            clang
+            cmake
+            ninja
+            emscripten
+            wasm-pack
+            nodejs
+
+            # Package managers
+            uv
+            pnpm
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+
+            # Nix helpers
+            nixd
+            nil
+          ];
+        };
+
+        packages.hello = hello;
+      }
+    );
+}
