b32830fefa71824d609e6f528a37fe28157ca01c
Arif Basharyar <saye.arif@gmail.com>
2026-05-14 21:20:36 +0300

    improved examples readability

--- projects/rust-app1/src/main.rs
+++ projects/rust-app1/src/main.rs
@@ -2,7 +2,7 @@ use thesis::cpp_lib2::add as cpp_add;
 use thesis::rust_lib1::add as rust_add;
 
 fn main() {
-  println!("Hello, world!");
+  println!("Hello, world from rust-app1!");
   println!("rust_add(2, 2) = {}", rust_add(2, 2));
   println!("cpp_add(2, 2) = {}", cpp_add(2, 2));
 }
